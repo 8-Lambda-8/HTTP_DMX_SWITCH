cb1bb9a5e19ccb01c80f015dbdc9c0ad6b42b189
8-Lambda-8 <j.wasle111@gmail.com>
2024-06-07 12:01:00 +0200

    request all path

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,7 @@
 
 char host[] = "red-old.lambda8.at";
 
-char pathKiosk[] = "/gw/kiosk/";
-char pathUnten[] = "/gw/unten/";
-char pathLoge[] = "/gw/loge/";
-char pathTreppe[] = "/gw/treppe/";
-
-char* paths[] = {pathKiosk, pathUnten, pathLoge, pathTreppe};
+char path[] = "/gw/all/";
 
 // Set the static IP address to use if the DHCP fails to assign
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
@@ -19,10 +14,8 @@ IPAddress ip(192, 168, 0, 177);
 IPAddress myDns(192, 168, 0, 1);
 
 EthernetClient client;
-EthernetServer server(80);
 
 void httpRequest(char* host, char* path);
-void httpResponse(EthernetClient client);
 
 void writeChannels(uint16_t channels[], uint8_t size, uint8_t data, boolean rgb = true);
 
@@ -51,7 +44,6 @@ void setup() {
     }
     Ethernet.begin(mac, ip, myDns);
   }
-  server.begin();
 
   delay(1000);
 }
@@ -59,59 +51,31 @@ void setup() {
 int StrToHex(const char str[]) { return (int)strtol(str, 0, 16); }
 
 uint32_t requestTimer = 0;
-uint8_t request = 0;
 void loop() {
-  if (millis() - requestTimer > 2000) {
+  if (millis() - requestTimer > 500) {
     requestTimer = millis();
-    if (client.connect(host, 80)) {
-      httpRequest(host, paths[request]);
-      request++;
-      if (request >= 4) request = 4;
-    }
+    httpRequest(host, path);
   }
 
-  int8_t x = -1;
   while (client.available()) {
-    String line = client.readStringUntil('\r');
+    client.readStringUntil('\r');
 
-    if (line.startsWith("\nlocation: kiosk")) x = 0;
-    if (line.startsWith("\nlocation: unten")) x = 1;
-    if (line.startsWith("\nlocation: loge")) x = 2;
-    if (line.startsWith("\nlocation: treppe")) x = 3;
+    String col = client.readStringUntil(',');
+    color[0] = StrToHex(col.substring(2, 4).c_str());
+    color[1] = StrToHex(col.substring(4, 6).c_str());
+    color[2] = StrToHex(col.substring(6, 8).c_str());
 
-    if (line.length() < 2 && x > -1) {
-      uint8_t data = client.readString().toInt();
-      writeChannels(channels[x], sizes[x], data, x == 0);
+    EEPROM.update(0, color[0]);
+    EEPROM.update(1, color[1]);
+    EEPROM.update(2, color[2]);
 
-      DMXSerial.write(512, 0);
-    };
+    writeChannels(channels[0], sizes[0], client.readStringUntil(',').toInt(), true);   // kiosk
+    writeChannels(channels[2], sizes[2], client.readStringUntil(',').toInt(), false);  // loge
+    writeChannels(channels[1], sizes[1], client.readStringUntil(',').toInt(), false);  // unten
+    writeChannels(channels[3], sizes[3], client.readStringUntil(',').toInt(), false);  // treppe
+
+    DMXSerial.write(512, 0);
   }
-
-  EthernetClient client = server.available();
-  if (client) {
-    while (client.connected()) {
-      if (client.available()) {
-        String line = client.readStringUntil('\r');
-        if (line.startsWith("GET /?color=%23")) {
-          color[0] = StrToHex(line.substring(15, 17).c_str());
-          color[1] = StrToHex(line.substring(17, 19).c_str());
-          color[2] = StrToHex(line.substring(19, 21).c_str());
-
-          EEPROM.update(0, color[0]);
-          EEPROM.update(1, color[1]);
-          EEPROM.update(2, color[2]);
-        }
-
-        if (line.length() < 2) {
-          httpResponse(client);
-          break;
-        }
-      }
-    }
-    delay(5);
-    client.stop();
-  }
-
   delay(1);
 }
 
@@ -135,31 +99,4 @@ void httpRequest(char* host, char* path) {
   client.println(host);
   client.println("Connection: close");
   client.println();
-}
-
-void httpResponse(EthernetClient client) {
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println("Connection: close");
-  client.println();
-
-  client.print("<!DOCTYPE html>");
-  client.print("<html><head>");
-  client.print("    <meta charset=\"UTF-8\" />");
-  client.print("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />");
-  client.print("    <title>Document</title>");
-  client.print("  </head>");
-  client.print("  <body>");
-  client.print("    <form>");
-  client.print("      <input type=\"color\" name=\"color\" value=\"#");
-  if (color[0] < 16) client.print("0");
-  client.print(color[0], 16);
-  if (color[1] < 16) client.print("0");
-  client.print(color[1], 16);
-  if (color[2] < 16) client.print("0");
-  client.print(color[2], 16);
-  client.print("\"/>");
-  client.print("      <input type=\"submit\" />");
-  client.print("    </form>");
-  client.println("</body></html>");
 }
